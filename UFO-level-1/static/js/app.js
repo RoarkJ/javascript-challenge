cd4d7ff38bdb8a4422c57efbb136a046b1224a81
Bryan <30875957+RoarkJ@users.noreply.github.com>
2020-09-24 16:25:52 -0600

    complete required protion of javascript assignment

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,31 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody=d3.select("tbody");
+function append_table(tableData) {
+    tableData.forEach((record)=> 
+    { var table_row=tbody.append("tr");
+    Object.values(record).forEach((value)=>
+    { var data=table_row.append("td");
+    data.text(value);
+    });
+    });
+};
+
+append_table(tableData);
+
+function filter_data() {
+    d3.event.preventDefault();
+    var date=d3.select("#datetime").property("value");
+    var filtered_data=tableData;
+    if (date) {
+        filtered_data=filtered_data.filter((row) => row.datetime===date);
+    };
+    tbody.html("");
+    append_table(filtered_data)
+};
+
+// d3.selectAll("#filter-btn").on("click",filter_data);
+var button=d3.selectAll("#filter-btn");
+button.on("click", filter_data);
+
